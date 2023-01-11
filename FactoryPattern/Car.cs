97b80fe07c4ec93faca75727558fd39ecadf0e1c
Schlabach <matthewshawn89@gmail.com>
2023-01-11 06:01:02 -0500

    intial commit

---
+++ FactoryPattern/Car.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryPattern
+{
+    class Car : IVehicle
+    {
+        public string Color { get; set; }
+        public bool IsDrivable { get; set; }
+
+        public void Drive()
+        {
+            Console.WriteLine($"Building a new Car!");
+        }
+    }
+}
---
+++ FactoryPattern/VehicleFactory.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryPattern
+{
+    class VehicleFactory
+    {
+        public static IVehicle GetVehicle(int wheelCount)
+        {
+            switch (wheelCount)
+            {
+                case 4:
+                    return new Car();
+                case 2:
+                     return new Motorcycle();
+                default:
+                    return new Car();
+            }
+        }
+    }
+}
